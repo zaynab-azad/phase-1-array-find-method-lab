79133fea182a8c0d267058ca2c0010491e5a67e2
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-23 22:09:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+  const reslt = record.find((element) => {
+    if (element.result == "W") {
+      return element;
+    }
+  });
+  return reslt != undefined ? reslt.year : undefined;
+}
